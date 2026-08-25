7da78cf09e29995533d062e77587bc918c7cc851
mimi <hajimidb@gmail.com>
2026-08-25 11:23:49 +0800

    簡易回傳輸入內容

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import streamlit as st
-st.title("hajimi")
+
+st.title("Welcome to my first Streamlit web")
+
+st.write("Hello Streamlit!")
+
+name = st.text_input("Insert your name:")
+
+if name:
+    st.write(f"Hi，{name}！")
+
+
+st.chat_input("Ask a question...")
